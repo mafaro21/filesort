19d74413791b1e3b69b1ccd2f761d56345b9fc47
mafaro21 <mafaro2105@gmail.com>
2024-05-07 00:27:13 +0200

    file type color and time

--- fileSorter.py
+++ fileSorter.py
@@ -1,10 +1,9 @@
-import os, shutil
+import os, shutil, time, re
+import eyed3
 
 def SortExtension(path):
-    # path = r"C:/Users/mafar/Pictures/4/"
 
     files_in_path = os.listdir(path)
-
 
     # --------------------SORTING BY EXTENSION----------------------------------
     extension_array = []
@@ -12,58 +11,69 @@ def SortExtension(path):
     final_extension_array = []
     folder_names = []
 
+    class colors:
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        DONE = '\033[95m'
+
     # separate filename from extension  
     for ext in files_in_path:
         split_names = os.path.splitext(ext)
-        extension_name = split_names[1]
+        raw_extension = split_names[1]
+        extension_name = raw_extension.lower()
         extension_array.append(extension_name)
-        
-    count = -1
-    # clean out folders from files  
-    for clean_ext in extension_array:
-        count = count + 1
-        if len(clean_ext) == 0:
-            del extension_array[count]
-            continue
-        # print(count)
+            
+        count = -1
+        # clean out folders from files  
+        for clean_ext in extension_array:
+            count = count + 1
+            if len(clean_ext) == 0:
+                del extension_array[count]
+                continue
+            # print(count)
 
-        clean_extension_array.append(clean_ext)
+            clean_extension_array.append(clean_ext)
 
-        # clean out duplicates
-        final_extension_array = list(set(clean_extension_array))
+            # clean out duplicates
+            final_extension_array = list(set(clean_extension_array))
 
-    #remove the . to create folder names
-    for folder in final_extension_array:
-        folder = folder + ' files'
-        folder = folder.replace('.','')
-        folder_names.append(folder)
+        #remove the . to create folder names
+        for folder in final_extension_array:
+            folder = folder + ' files'
+            folder = folder.replace('.','')
+            folder_names.append(folder)
 
-    # create folders if not already there
-    for loop in folder_names:
-        if not os.path.exists(path + loop):
-            os.makedirs(path + loop)
+        # create folders if not already there
+        for loop in folder_names:
+            if not os.path.exists(path + loop):
+                os.makedirs(path + loop)
 
-    files = 0
-    # sort files by extension
-    for file in files_in_path:
-        for file_ext in extension_array:
+        files = 0
+        start_time = time.time()
+        # sort files by extension
+        for file in files_in_path:
+            for file_ext in extension_array:
 
-            for folder in final_extension_array:
-                
-                if file_ext == folder:
-                    folder = folder + ' files/'
-                    folder = folder.replace('.','')
+                for folder in final_extension_array:
+                    
+                    if file_ext == folder:
+                        folder = folder + ' files/'
+                        folder = folder.replace('.','')
 
-                    if file_ext in file and not os.path.exists(path + folder + file):
-                        files += 1
-                        shutil.move(path + file, path + folder + file)
+                        if file_ext in file and not os.path.exists(path + folder + file):
+                            files += 1
+                            shutil.move(path + file, path + folder + file)
 
-    if files >= 1:
-        print(files , 'files have been sorted')
-    else:
-        print('There has been an error in sorting')
+        end_time = time.time()
+        elapsed_time = end_time - start_time
 
-    # ---------BY FILE SIZE-----------------
-    # test = os.stat(path + ext)
-    #     final = test.st_size
-    #     print(final)
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+
+        if files >= 1:
+            print(files , colors.DONE + 'files have been sorted' + colors.ENDC)
+            print(colors.OKGREEN + 'Process took', minutes, "minute(s) &", seconds, 'seconds to sort'+ colors.ENDC)
+        else:
+            print(colors.FAIL + 'There has been an error sorting' + colors.ENDC)
